4ecaf513e9ec27c379f4e9066d78d0c01a75552c
Marcel Kempf <gloeckchenxvx@gmail.com>
2015-07-13 20:58:51 +0200

    add vote remote method

--- common/models/survey.js
+++ common/models/survey.js
@@ -1,11 +1,55 @@
 module.exports = function(Survey) {
-    Survey.vote = function(id, cb) {
-        self.findOne(id, function(err, survey) {
-            console.log(err, survey);
+    Survey.vote = function(id, voting, cb) {
+        var mongodb = require('mongodb'),
+            query = {
+                id: id
+            },
+            answers,
+            chosenAnswer,
+            newChosenAnswer;
+        this.findOne({where: query}, function(err, survey) {
+            if (!survey) return cb(null, false);
+
+            // Iterate through voted questions
+            Object.keys(voting).forEach(function(questionId) {
+                answers = voting[questionId];
+                survey.questions.findById(questionId, function(err, question) {
+                    if (!question) return cb(null, false);
+
+                    // iterate through voted answers
+                    Object.keys(answers).forEach(function(answerId) {
+                        question.answers.findById(answerId, function(err, answer) {
+                            if (!answer) return cb(null, false);
+
+                            // Update chosenAnswers
+                            question.chosenAnswers({where: {answerId: mongodb.ObjectID(answerId)}}, function(err, chosenAnswers) {
+                                var count = chosenAnswers.length ? chosenAnswers[0].count : 0;
+                                count++;
+
+                                if (chosenAnswers.length) {
+                                    chosenAnswer = chosenAnswers[0];
+                                    chosenAnswer.count = count;
+                                    chosenAnswer.save({}, function(err, instance) {
+                                        if (!err && instance) return cb(null, true);
+                                    });
+                                    return;
+                                }
+                                newChosenAnswer = {
+                                    questionId: question.id,
+                                    answerId: answer.id,
+                                    count: count
+                                };
+                                question.chosenAnswers.create(newChosenAnswer, function(err, createdChosenAnswer) {
+                                    if (createdChosenAnswer) return cb(null, true);
+                                });
+                            });
+                        });
+                    });
+                });
+            });
         });
 
-        var response = true;
-        cb(null, response);
+        // cb(null, response);
     };
     Survey.remoteMethod(
         'vote',
@@ -18,6 +62,14 @@ module.exports = function(Survey) {
                     required: true,
                     http: {
                         source: 'query'
+                    }
+                },
+                {
+                    arg: 'data',
+                    type: 'object',
+                    required: true,
+                    http: {
+                        source: 'body'
                     }
                 },
             ],
